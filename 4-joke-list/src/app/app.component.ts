872cd03032743f96fd47b99fea960ee4c91bdce7
Szamurajpalanta <s_janos@outlook.com>
2022-03-15 18:11:44 +0100

    move list to its own component

--- 4-joke-list/src/app/app.component.ts
+++ 4-joke-list/src/app/app.component.ts
@@ -24,26 +24,4 @@ export class AppComponent {
     jokes.push(this.newJoke);
     this.initializeNewJoke();
   }
-
-  countUpvotes(): number {
-    let sum = 0;
-    for (let index = 0; index < jokes.length; index++) {
-      const joke = jokes[index];
-      sum += joke.upvotes;
-    }
-    return sum;
-  }
-
-  countDownvotes(): number {
-    let sum = 0;
-    for (let index = 0; index < jokes.length; index++) {
-      const joke = jokes[index];
-      sum += joke.downvotes;
-    }
-    return sum;
-  }
-
-  countAllVotes(): number {
-    return this.countUpvotes() + this.countDownvotes();
-  }
 }
--- 4-joke-list/src/app/list/list.component.ts
+++ 4-joke-list/src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { jokes } from '../joke';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+  jokes = jokes;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  countUpvotes(): number {
+    let sum = 0;
+    for (let index = 0; index < jokes.length; index++) {
+      const joke = jokes[index];
+      sum += joke.upvotes;
+    }
+    return sum;
+  }
+
+  countDownvotes(): number {
+    let sum = 0;
+    for (let index = 0; index < jokes.length; index++) {
+      const joke = jokes[index];
+      sum += joke.downvotes;
+    }
+    return sum;
+  }
+
+  countAllVotes(): number {
+    return this.countUpvotes() + this.countDownvotes();
+  }
 }
