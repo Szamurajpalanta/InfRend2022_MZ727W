aa77561c0c00d14ec6d5879dea6f3125e0fa69f7
Szamurajpalanta <s_janos@outlook.com>
2022-03-15 01:59:22 +0100

    add joke interface and array

---
+++ joke-list/src/app/joke.ts
@@ -0,0 +1,27 @@
+export interface Joke {
+    user: string;
+    text: string;
+    upvotes: number;
+    downvotes: number;
+}
+
+export const jokes: Joke[] = [
+    {
+        user: "Aunt Arctic",
+        text: "Why don't you see a penguin in the U.K.?<br/>Because they're afraid of Wales!",
+        upvotes: 20,
+        downvotes: 10
+    },
+    {
+        user: "Rockhopper",
+        text: "What bird can write underwater? A ball point PENguin!",
+        upvotes: 10,
+        downvotes: 100
+    },
+    {
+        user: "Sensei",
+        text: "What do you call a penguin in the desert? Lost.",
+        upvotes: 200,
+        downvotes: 20
+    }
+]
