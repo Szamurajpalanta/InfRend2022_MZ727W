ce6c67c753de2f7ec06e4bc1af8eea8a27983f98
Szamurajpalanta <s_janos@outlook.com>
2022-03-15 01:32:04 +0100

    add empty components

--- joke-list/src/app/app.module.ts
+++ joke-list/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
