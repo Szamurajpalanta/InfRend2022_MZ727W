dbec5da01d30fbbe839b3ab29666b5af88b13d12
Szamurajpalanta <s_janos@outlook.com>
2022-04-18 14:45:22 +0200

    add navbar, implement routing

--- 5-github-api/src/app/app-routing.module.ts
+++ 5-github-api/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepoDetailsComponent } from './repo-details/repo-details.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-search', component: UserSearchComponent },
+  { path: 'user-detail/:id', component: UserDetailsComponent },
+  { path: 'repo-search', component: RepoSearchComponent },
+  { path: 'repo-detail/:id', component: RepoDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 5-github-api/src/app/app.module.ts
+++ 5-github-api/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { RepoDetailsComponent } from './repo-details/repo-details.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSearchComponent,
+    RepoSearchComponent,
+    RepoDetailsComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
