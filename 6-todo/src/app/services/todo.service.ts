2bde84898162b1f858e786bda3b6ccfa08a23e0c
Szamurajpalanta <s_janos@outlook.com>
2022-05-13 05:30:20 +0200

    create todo service

--- 6-todo/src/app/services/todo.service.ts
+++ 6-todo/src/app/services/todo.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Todo } from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSubject(id: number) {
+    return lastValueFrom(this.http.get<Todo[]>(`http://localhost:3000/api/todo/${id}`));
+  }
+
+  getSubjects() {
+    return lastValueFrom(this.http.get<Todo[]>('http://localhost:3000/api/todo'));
+  }
+
+  /*
+  searchSubjects(query: string) {
+    return lastValueFrom(this.http.get<Todo[]>('http://localhost:3000/api/search/subjects', {
+      params: {
+        search: query
+      }
+    }));
+  }
+  */
+
+  createSubject(subject: Todo) {
+    return lastValueFrom(this.http.post<Todo>('http://localhost:3000/api/todo', subject));
+  }
+
+  updateSubject(subject: Todo) {
+    return lastValueFrom(this.http.put<Todo>('http://localhost:3000/api/todo', subject));
+  }
+
+  deleteSubject(id: number) {
+    return lastValueFrom(this.http.delete<Todo>(`http://localhost:3000/api/todo/${id}`));
+  }
 }
