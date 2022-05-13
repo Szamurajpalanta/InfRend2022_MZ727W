2781972fc8595e5eb537187d99e29d09c61ae5f5
Szamurajpalanta <s_janos@outlook.com>
2022-05-13 05:28:09 +0200

    create empty components

--- 6-todo/src/app/app.module.ts
+++ 6-todo/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent
   ],
   imports: [
     BrowserModule,
